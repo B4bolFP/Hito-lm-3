81756b423a5ca7b96f16db3abd55cb8a65829b3e
B4bolFP <mario.lopezl@campusfp.es>
2023-05-04 10:48:07 +0200

    a

---
+++ js/index.js
@@ -0,0 +1,327 @@
+let encuestas = [
+    {
+      "fecha": "27/04/2023 10:34:08",
+      "nombre": "asd",
+      "apellidos": "asd",
+      "centro": "Campusfp",
+      "edad": "12/04/2003",
+      "preferencias": "Instagram, Twitter",
+      "uso_principal": "Entretenimiento",
+      "diario": "Sí",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "29/04/2023 14:01:48",
+      "nombre": "Rodrigo",
+      "apellidos": "Pozo Sánchez",
+      "centro": "Humanes",
+      "edad": "18/07/2003",
+      "preferencias": "Instagram, Tik Tok",
+      "uso_principal": "Entretenimiento, Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "30/04/2023 9:54:47",
+      "nombre": "Diego",
+      "apellidos": "García Luque",
+      "centro": "Getafe",
+      "edad": "17/06/2004",
+      "preferencias": "Instagram, Tik Tok, Twitter",
+      "uso_principal": "Entretenimiento, Trabajo",
+      "diario": "Sí",
+      "uso": "Pierdo la noción del tiempo y me dan las tantas viendo videos de gatos y cosas random.",
+      "alternativas": "Trabajar, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "30/04/2023 12:04:06",
+      "nombre": "Chedey",
+      "apellidos": "Gallego Gabaldón",
+      "centro": "CampusFP Getafe",
+      "edad": "27/03/2004",
+      "preferencias": "Instagram",
+      "uso_principal": "Entretenimiento, Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "Sí",
+      "uso": "Entre 10 y 30 minutos",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta, Trabajar, Ocio y entretenimiento, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "1/05/2023 21:29:40",
+      "nombre": "Daniel",
+      "apellidos": "Mier",
+      "centro": "Humanes",
+      "edad": "21/06/2004",
+      "preferencias": "Instagram",
+      "uso_principal": "Entretenimiento, Conocer gente",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 9:08:40",
+      "nombre": "David",
+      "apellidos": "Díez García",
+      "centro": "CampusFP Humanes",
+      "edad": "9/05/2003",
+      "preferencias": "Instagram",
+      "uso_principal": "Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta, Trabajar, Ocio y entretenimiento, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 9:56:43",
+      "nombre": "mAURO",
+      "apellidos": "Ortiz Gamonal",
+      "centro": "getafe",
+      "edad": "22/11/2023",
+      "preferencias": "Instagram",
+      "uso_principal": "Entretenimiento",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 11:24:58",
+      "nombre": "Raul",
+      "apellidos": "Rodelgo",
+      "centro": "HUmanes",
+      "edad": "21/11/2003",
+      "preferencias": "Instagram, Tik Tok",
+      "uso_principal": "Entretenimiento, Trabajo, Aprendizaje (ver tutoriales o seguir a personas que admiras), Conocer gente",
+      "diario": "Sí",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta, Trabajar, Disfrutar de la compañía de amigos y familiares,",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 11:26:12",
+      "nombre": "Nerea",
+      "apellidos": "Ruiz",
+      "centro": "Humanes",
+      "edad": "11/03/2004",
+      "preferencias": "Instagram, Tik Tok",
+      "uso_principal": "Entretenimiento, Conocer gente",
+      "diario": "Sí",
+      "uso": "Pierdo la noción del tiempo y me dan las tantas viendo videos de gatos y cosas random.",
+      "alternativas": "Ocio y entretenimiento, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 11:41:33",
+      "nombre": "Mario",
+      "apellidos": "Lopez Llamas",
+      "centro": "Humanes",
+      "edad": "26/04/2003",
+      "preferencias": "Instagram, Twitter",
+      "uso_principal": "Entretenimiento, Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 11:44:35",
+      "nombre": "Vladislav",
+      "apellidos": "Slavev",
+      "centro": "Humanes",
+      "edad": "1/01/2073",
+      "preferencias": "Forocoches",
+      "uso_principal": "Entretenimiento, Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "Sí",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta, Ocio y entretenimiento, Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 17:39:32",
+      "nombre": "Jose",
+      "apellidos": "Llera Fernandez",
+      "centro": "Humanes",
+      "edad": "23/08/2004",
+      "preferencias": "Tik Tok",
+      "uso_principal": "Entretenimiento, Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "No",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 17:40:24",
+      "nombre": "inventando1",
+      "apellidos": "inventando1",
+      "centro": "Getafe",
+      "edad": "12/05/2002",
+      "preferencias": "Snapchat",
+      "uso_principal": "Entretenimiento",
+      "diario": "Sí",
+      "uso": "Entre 10 y 30 minutos",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 17:43:09",
+      "nombre": "inventado2",
+      "apellidos": "inventado2",
+      "centro": "Humano",
+      "edad": "18/02/1986",
+      "preferencias": "Facebook",
+      "uso_principal": "Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "No",
+      "uso": "Entre 10 y 30 minutos",
+      "alternativas": "Trabajar",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 17:48:04",
+      "nombre": "inventado3",
+      "apellidos": "inventado3",
+      "centro": "Getafe",
+      "edad": "7/06/2004",
+      "preferencias": "Twitter",
+      "uso_principal": "Entretenimiento",
+      "diario": "Sí",
+      "uso": "Pierdo la noción del tiempo y me dan las tantas viendo videos de gatos y cosas random.",
+      "alternativas": "Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 17:49:38",
+      "nombre": "inventado4",
+      "apellidos": "inventado4",
+      "centro": "Humanes",
+      "edad": "21/11/2003",
+      "preferencias": "Instagram",
+      "uso_principal": "Conocer gente",
+      "diario": "Sí",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 17:53:14",
+      "nombre": "inventado5",
+      "apellidos": "inventado5",
+      "centro": "Humanes",
+      "edad": "3/01/2000",
+      "preferencias": "Snapchat",
+      "uso_principal": "Aprendizaje (ver tutoriales o seguir a personas que admiras)",
+      "diario": "No",
+      "uso": "Entre 10 y 30 minutos",
+      "alternativas": "Estudiar, aprender cosas de forma autodidacta",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 17:58:13",
+      "nombre": "inventado6",
+      "apellidos": "inventado6",
+      "centro": "Getafe",
+      "edad": "14/04/2001",
+      "preferencias": "Tik Tok",
+      "uso_principal": "Entretenimiento",
+      "diario": "No",
+      "uso": "Entre 30 minutos y 1 hora",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "No"
+    },
+    {
+      "fecha": "3/05/2023 17:59:12",
+      "nombre": "Inventado7",
+      "apellidos": "Inventado7",
+      "centro": "Humanes",
+      "edad": "21/09/1995",
+      "preferencias": "Snapchat",
+      "uso_principal": "Trabajo",
+      "diario": "Sí",
+      "uso": "Entre 1 y 2 horas",
+      "alternativas": "Disfrutar de la compañía de amigos y familiares",
+      "abstiencia": "Sí"
+    },
+    {
+      "fecha": "3/05/2023 18:00:05",
+      "nombre": "Inventado8",
+      "apellidos": "Inventado8",
+      "centro": "Humanes",
+      "edad": "28/01/2000",
+      "preferencias": "Tik Tok",
+      "uso_principal": "Conocer gente",
+      "diario": "Sí",
+      "uso": "Pierdo la noción del tiempo y me dan las tantas viendo videos de gatos y cosas random.",
+      "alternativas": "Ocio y entretenimiento",
+      "abstiencia": "No"
+    }
+]
+
+let placeholder = document.querySelector(".body-section-container");
+let out = "";
+let cont = 1;
+for(let campo of encuestas){
+    out += `
+        <div class="card">
+            <h2>Encuesta ${cont}</h2>
+            <div class="fields">
+                <div class="field">
+                    <h3>Fecha</h3>
+                    <p>${campo.fecha}</p>
+                </div>
+                <div class="field">
+                    <h3>Nombre</h3>
+                    <p>${campo.nombre}</p>
+                </div>
+                <div class="field">
+                    <h3>Apellidos</h3>
+                    <p>${campo.apellidos}</p>
+                </div>
+                <div class="field">
+                    <h3>Edad</h3>
+                    <p>${campo.edad}</p>
+                </div>
+            </div>
+            <div class="fields">
+                <div class="field">
+                    <h3>Centro</h3>
+                    <p>${campo.centro}</p>
+                </div>
+                <div class="field">
+                    <h3>Preferencias</h3>
+                    <p>${campo.preferencias}</p>
+                </div>
+                <div class="field">
+                    <h3>Uso_principal</h3>
+                    <p>${campo.uso_principal}</p>
+                </div>
+
+
+            </div>
+            <div class="fields">
+                <div class="field">
+                    <h3>Diario</h3>
+                    <p>${campo.diario}</p>
+                </div>
+                <div class="field">
+                    <h3>Uso</h3>
+                    <p>${campo.uso}</p>
+                </div>
+                <div class="field">
+                    <h3>Alternativas</h3>
+                    <p>${campo.alternativas}</p>
+                </div>
+                <div class="field">
+                    <h3>Abstiencia</h3>
+                    <p>${campo.abstiencia}</p>
+                </div>
+            </div>
+        </div>
+    `;
+    cont++;
+}
+placeholder.innerHTML = out;
